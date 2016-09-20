8ed21e7a7df4f7b53de4ca40ecf970f64ca80354
Tol182 <toldark182@gmail.com>
2016-09-20 20:43:01 +0300

    added basic night vision logic

--- data/turn-off.js
+++ data/turn-off.js
@@ -1,5 +1,12 @@
+function turnOff() {
+    document.body.style.filter = "invert(0%)";
+
+    consoleOut();
+}
+
 function consoleOut() {
     console.log("OFF");
 }
 
+turnOff();
 consoleOut();
--- data/turn-on.js
+++ data/turn-on.js
@@ -1,5 +1,12 @@
+function turnOn() {
+    document.body.style.filter = "invert(100%)";
+
+    consoleOut();
+}
+
 function consoleOut() {
     console.log("ON");
 }
 
+turnOn();
 consoleOut();
