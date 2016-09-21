fbf58585e61a922da5292f2f873385f0cbd2d715
Tol182 <toldark182@gmail.com>
2016-09-21 20:42:00 +0300

    added doc comments

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // TODO add different night modes
-// TODO documentation comments
-// TODO add style to panel
-// TODO add icons to panel
 
+/*Firefox SDK imports*/
 var buttons = require('sdk/ui/button/action');
 var panels = require("sdk/panel");
 var self = require("sdk/self");
 var tabs = require("sdk/tabs");
 var toggle = require('sdk/ui/button/toggle');
 
+/*Add-on menu button*/
 var button = toggle.ToggleButton({
     id: "nightvision-menu-button",
     label: "Open NightVision menu",
@@ -20,14 +19,17 @@ var button = toggle.ToggleButton({
     onChange: handleChange
 });
 
+/*Add-on panel*/
 var panel = panels.Panel({
     contentURL: self.data.url("panel.html"),
     contentScriptFile: self.data.url("panel-buttons.js"),
     onHide: handleHide
 });
 
+/*Panel width in px*/
 panel.width = 200;
 
+/*Panel show*/
 function handleChange(state) {
     if (state.checked) {
         panel.show({
@@ -36,11 +38,12 @@ function handleChange(state) {
     }
 }
 
+/*Panel hide*/
 function handleHide() {
     button.state('window', {checked: false});
 }
 
-
+/*Get user click from panel*/
 panel.port.on("invertM", function (panelmethod) {
     tabs.activeTab.attach({
         contentScriptFile: self.data.url(panelmethod)
