bd8877c5228544353ca43693a4d8f587a27c9a7c
Tol182 <toldark182@gmail.com>
2016-09-21 21:48:43 +0300

    improved modes switching logic

--- data/panel-buttons.js
+++ data/panel-buttons.js
@@ -1,30 +1,30 @@
 /*Global flag for all modes*/
-var isEnabled = false;
+var currentMethod = "off";
 
 /*Listeners for img - 'buttons'*/
 document.getElementById("invert").addEventListener("click", invert);
 document.getElementById("smartInvert").addEventListener("click", smartInvert);
 
 function invert() {
-    if (isEnabled) {
-        self.port.emit("invertM", "invert-turn-off.js");
-
-        isEnabled = false;
-    } else {
+    if (currentMethod != "invert") {
         self.port.emit("invertM", "invert-turn-on.js");
 
-        isEnabled = true;
+        currentMethod = "invert";
+    } else {
+        self.port.emit("invertM", "invert-turn-off.js");
+
+        currentMethod = "off";
     }
 }
 
 function smartInvert() {
-    if (isEnabled) {
-        self.port.emit("invertM", "smart-invert-turn-off.js");
-
-        isEnabled = false;
-    } else {
+    if (currentMethod != "smartinvert") {
         self.port.emit("invertM", "smart-invert-turn-on.js");
 
-        isEnabled = true;
+        currentMethod = "smartinvert";
+    } else {
+        self.port.emit("invertM", "smart-invert-turn-off.js");
+
+        currentMethod = "off";
     }
 }
