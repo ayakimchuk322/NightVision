6c014858369d3418ecec9b740a2d6f929eb90bd8
Tol182 <toldark182@gmail.com>
2016-09-26 17:04:52 +0300

    fixed wrong colors after turning off mode

--- data/matrix-turn-off.js
+++ data/matrix-turn-off.js
@@ -1,6 +1,20 @@
 function turnOff() {
+
+    // Remove stylesheet with matrix mode styles
     var matrixStyle = document.getElementById("matrixstyleid");
     matrixStyle.parentNode.removeChild(matrixStyle);
+
+    // Reload all the css without reloading page itself
+    // to restore removed rules
+    var links = document.getElementsByTagName("link");
+    for (var i = 0; i < links.length; i++) {
+
+        var link = links[i];
+
+        if (link.rel === "stylesheet") {
+            link.href += "?";
+        }
+    }
 
     consoleOut();
 }
