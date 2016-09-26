0c3e1bb655a4c5a7c894a2f57a5a078c047f71bb
Tol182 <toldark182@gmail.com>
2016-09-26 19:17:48 +0300

    minor tweak

--- data/matrix-turn-on.js
+++ data/matrix-turn-on.js
@@ -5,6 +5,11 @@ function turnOn() {
     for (var i = 0; i < allElements.length; i++) {
         allElements[i].style.removeProperty("backgroundColor");
         allElements[i].style.removeProperty("color");
+        allElements[i].style.removeProperty("border-color");
+        allElements[i].style.removeProperty("border-top-color");
+        allElements[i].style.removeProperty("border-right-color");
+        allElements[i].style.removeProperty("border-bottom-color");
+        allElements[i].style.removeProperty("border-left-color");
     }
 
     var matrixStyle = document.createElement("style");
@@ -16,9 +21,12 @@ function turnOn() {
     var matrixSheet = matrixStyle.sheet;
 
     matrixSheet.insertRule("* {background-color: black !important}", 0);
-    matrixSheet.insertRule("body {background-color: black !important}", 0);
     matrixSheet.insertRule("* {color: green !important}", 0);
-    matrixSheet.insertRule("body {color: green !important}", 0);
+    matrixSheet.insertRule("* {border-color: green; !important}", 0);
+    matrixSheet.insertRule("* {border-top-color: green; !important}", 0);
+    matrixSheet.insertRule("* {border-right-color: green; !important}", 0);
+    matrixSheet.insertRule("* {border-bottom-color: green; !important}", 0);
+    matrixSheet.insertRule("* {border-left-color: green; !important}", 0);
 
     consoleOut();
 }
