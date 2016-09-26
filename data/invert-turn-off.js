3e2bc142bddca1de11eecb4b8c4b21f336df30df
Tol182 <toldark182@gmail.com>
2016-09-26 18:13:26 +0300

    greatly improved inverting logic, added comments

--- data/invert-turn-off.js
+++ data/invert-turn-off.js
@@ -1,8 +1,30 @@
 function turnOff() {
+
+    // Disable invert filter
     document.documentElement.style.filter = "invert(0%)";
 
-    var invertStyle = document.getElementById("invertstyleid");
-    invertStyle.parentNode.removeChild(invertStyle);
+    // Remove stylesheet with invert mode styles
+    var matrixStyle = document.getElementById("invertstyleid987");
+    matrixStyle.parentNode.removeChild(matrixStyle);
+
+    // Remove added custom class
+    var allElements = document.getElementsByTagName("*");
+
+    for (var i = 0; i < allElements.length; i++) {
+        allElements[i].classList.remove("TransToWhite987");
+    }
+
+    // Reload all the css without reloading page itself
+    // to restore removed rules
+    var links = document.getElementsByTagName("link");
+    for (var i = 0; i < links.length; i++) {
+
+        var link = links[i];
+
+        if (link.rel === "stylesheet") {
+            link.href += "?";
+        }
+    }
 
     consoleOut();
 }
--- data/invert-turn-on.js
+++ data/invert-turn-on.js
@@ -1,17 +1,30 @@
 function turnOn() {
 
+    // Apply invert filter
     document.documentElement.style.filter = "invert(100%)";
 
+    // Mark elements with custom class - those with transparent bg
+    var allElements = document.getElementsByTagName("*");
+
+    for (var i = 0; i < allElements.length; i++) {
+        var bg = window.getComputedStyle(allElements[i]).getPropertyValue("background-color");
+
+        if (bg == "transparent") {
+            allElements[i].classList.add("TransToWhite987");
+        }
+    }
+
+    // Create stylesheet
     var invertStyle = document.createElement("style");
     var invertStyleId = document.createAttribute("id");
-    invertStyleId.value = "invertstyleid";
+    invertStyleId.value = "invertstyleid987";
     invertStyle.setAttributeNode(invertStyleId);
     document.head.appendChild(invertStyle);
 
     var invertSheet = invertStyle.sheet;
 
-    invertSheet.insertRule("* {background-color: white !important}", 0);
-    invertSheet.insertRule("body {background-color: white !important}", 0);
+    // Add rule for custom class - replace transparent bg with white
+    invertSheet.insertRule(".TransToWhite987 {background-color: white !important}", 0);
 
     consoleOut();
 }
